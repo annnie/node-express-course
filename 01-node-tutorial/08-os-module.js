a5cf1b73a6cad511943ef7699809b05c4162b31b
Annie Gerbeth <anniegerbeth@Anns-Mac.local>
2023-04-16 15:36:54 -0400

    Node and express tutorial portion done. First half of task manager api done

--- 01-node-tutorial/08-os-module.js
+++ 01-node-tutorial/08-os-module.js
@@ -14,3 +14,4 @@ const currentOS = {
   freeMem: os.freemem(),
 }
 console.log(currentOS)
+console.log(`the os release is: ${currentOS.release}`)
--- 01-node-tutorial/1-event-loop-examples/3-setInterval.js
+++ 01-node-tutorial/1-event-loop-examples/3-setInterval.js
@@ -1,3 +1,4 @@
+
 setInterval(() => {
   console.log('hello world')
 }, 2000)
